5fcbcad6ea1223aaaf85b6c835f8e7cdc75ff21d
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-01-06 14:42:38 -0500

    Added other source files

--- src/singlebend_needleshape.m
+++ src/singlebend_needleshape.m
@@ -4,14 +4,26 @@
 %
 % - written by: Dimitri Lezcano
 
-function pos = singlebend_needleshape(curvatures, aa_tip_locs, L)
+function pos = singlebend_needleshape(curvatures, aa_tip_locs, L, kc_i, w_init_i, theta0)
 % Input:
 %   - curvatures: list of x-y curvatures measured at each of the AA locations
-%           ( a #AA x 2 matrix )
+%           ( a #AA x 2 matrix ) (1/m)
 %   - aa_locs: list of the active area locations (measured from the tip of
-%   the needle)
-%   - L: the needle length
-%
+%   the needle) corresponding w/ rows of curvatures
+%   - L: the needle length (mm)
+%   - kc_i: the initial guess of kappa_c (double)
+%   - w_init_i: the initial guess of w_init (3 x 1 vector, default is [kc_init; 0; 0])
+    
+    %% Arguments
+    arguments
+        curvatures (:, 2) {mustBeNumeric};
+        aa_tip_locs (1,:) {mustBeNumeric};
+        L double;
+        kc_i double;
+        w_init_i (3, 1) {mustBeNumeric} = [kc_i; 0; 0]; 
+        theta0 double = 0;
+    end
+
     %% material properties
     % Stainless Steel 304
     Emod = 200e9*1e-6; % 200 GPa, conversion from N/m^2 to N/mm^2
@@ -35,23 +47,49 @@ function pos = singlebend_needleshape(curvatures, aa_tip_locs, L)
     s = 0:ds:L;
     
     % get the arclength indices that are valid
-    s_idx_aa = [];
-    curvs_aa = [];
-    for i = 1:length(aa_base_locs)
-        aa_loc = aa_base_locs(i);
-        idx = find(s == aa_loc);
-        if ~isempty(idx)
-            s_idx_aa = [s_idx_aa; idx];
-            curvs_aa = [curvs_aa; curvatures(i,:), 0];
-        end
-    end
-    
-    % grab each of the valid arclengths and curvatures
+    aa_base_locs_valid = aa_base_locs(aa_base_locs >= 0);
+    [~, s_idx_aa] = min(abs(s' - aa_base_locs_valid));
+    curvs_aa = curvatures(aa_base_locs >= 0, :)*1e-3; % convert curvatures to 1/mm
     s_aa = s(s_idx_aa);
-%     curvs_aa = curvatures(s_idx_aa, :);
-    curvs_aa = [curvs_aa; zeros(size(curvs_aa, 1), 1)];
+        
+    
+    %% Determine w_init and kc from measured curvatures (optimization)
+    % intrinsic curvature values
+    k0 = kc_i * (1 - s/L).^2;
+    w0 = [k0; zeros(2, length(s))];
+    
+    k0_prime = -2*kc_i/L * (1 - s/L);
+    w0_prime = [k0_prime; zeros(2, length(s))];
+    
+    % initial cost values
+    eta = [w_init_i; kc_i];
+%     wv = fn_intgEP_v1(w_init_i, w0, w0_prime, 0, ds, length(s), B, Binv);
+    scalef0 = 1;
+    Cval = costfn_shape_singlebend(eta, curvs_aa', s_idx_aa, ds, length(s), B, Binv, scalef0);
+    scalef = 1/Cval;
+    
+    % optimization
+    x0 = eta; % initial value
+    LB = [-0.01*ones(3,1);0]; % lower bound
+    UB = [0.01*ones(3,1);0.01]; % upper bound
+    
+    oldopts = optimset('fmincon');
+    options = optimset(oldopts,'Algorithm','interior-point','TolFun',1e-6,'TolX',1e-8,...
+        'MaxFunEvals',10000);
+    [x, fval, exitflag] = fmincon( @(x) costfn_shape_singlebend(x, curvs_aa',...
+        s_idx_aa, ds, length(s), B, Binv, scalef),...
+        x0, [], [], [], [], LB, UB, [], options);
+    
+    % unpack optimization results
+    w_init = x(1:3);
+    kc = x(4);
+    
+    %% Generate needle shape
+    w0 = kc * (1 - s/L).^2;
+    w0_prime = -2*kc/L * (1 - s/L);
     
     
-    %% Determine w_init and kc from measured curvatures
     
+    
+end
     
